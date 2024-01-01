edf1d06f0a48d1ca61514ff6d97ac2a4fc6f6a4c
DOriginalSamxie <samadrufai@gmail.com>
2024-01-02 00:00:01 +0100

    first commit

---
+++ controls.js
@@ -0,0 +1,12 @@
+class Controls {
+  constructor() {
+    this.foward = false
+    this.left = false
+    this.right = false
+    this.reverse = false
+
+    this.#addKeyboardListerners()
+  }
+
+  #addKeyboardListerners() {}
+}
